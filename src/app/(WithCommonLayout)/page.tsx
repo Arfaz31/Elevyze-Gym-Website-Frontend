cb126cc9cb7efb8f3c3c8cc2d13a4f83137c6cb2
Arfaz31 <arfazahamed31@gmail.com>
2025-06-27 23:31:14 +0600

    FeaturedClass

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
 
+import FeaturedClass from "./_component/FeaturedClass";
 import KnowUs from "./_component/KnowUs";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <KnowUs />
+      <FeaturedClass />
     </div>
   );
 }
